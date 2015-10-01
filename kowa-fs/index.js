053550607b9b63c3c6dc1cf85f901050ae172629
Riadh Chtara <riadh.chtara@gmail.com>
2015-10-01 20:15:50 +0200

    add methods

--- kowa-fs/index.js
+++ kowa-fs/index.js
@@ -6,8 +6,54 @@ var fs = require('fs');
 // external dependencies
 module.exports = function () {
   var KowaFs = Object.create(fs);
-  KowaFs.renameAsync = function(oldPath, newPath, callback) {
-     return new Promise(
+
+
+
+
+  KowaFs.appendFileAsync = function(filename, data, options) {
+    return new Promise(
+      function(resolve, reject) {
+        fs.appendFile(filename, data, options, function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }
+    );
+  };
+
+  KowaFs.chownAsync = function(path, uid, gid) {
+    return new Promise(
+      function(resolve, reject) {
+        fs.chown(path, uid, gid, function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }
+    );
+  };
+
+  KowaFs.ftruncateAsync = function(fd, len) {
+    return new Promise(
+      function(resolve, reject) {
+        fs.ftruncate(fd, len, function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }
+    );
+  };
+
+  KowaFs.renameAsync = function(oldPath, newPath) {
+    return new Promise(
       function(resolve, reject) {
         fs.rename(oldPath, newPath, function (err) {
           if (err) {
@@ -16,7 +62,37 @@ module.exports = function () {
             resolve();
           }
         });
-      });
+      }
+    );
   };
+
+  KowaFs.truncateAsync = function(path, len) {
+    return new Promise(
+      function(resolve, reject) {
+        fs.ftruncate(path, len, function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }
+    );
+  };
+
+  KowaFs.writeFileAsync = function(filename, data, options) {
+    return new Promise(
+      function(resolve, reject) {
+        fs.writeFile(filename, data, options, function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }
+    );
+  };
+
   return KowaFs;
 };
--- test/test.js
+++ test/test.js
@@ -7,8 +7,12 @@ var KowaFs = require('../kowa-fs')();
 
 Kowa.spawn(function* () {
   try {
+    yield KowaFs.writeFileAsync('/tmp/f1', 'content');
+    yield KowaFs.appendFileAsync('/tmp/f2', 'content');
+    yield KowaFs.ftruncateAsync('/tmp/f1', 5);
+    yield KowaFs.truncateAsync('/tmp/f1', 1);
     yield KowaFs.renameAsync('/tmp/f1', '/tmp/f2');
   } catch (e) {
-    console.log(e);
+    assert.ok(false);
   }
 });
