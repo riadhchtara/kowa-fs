c65688f1280967810fcff148047672517357f5b2
Riadh Chtara <riadh.chtara@gmail.com>
2015-10-02 17:56:46 +0200

    test

--- test/test.js
+++ test/test.js
@@ -7,12 +7,14 @@ var KowaFs = require('../kowa-fs')();
 
 Kowa.spawn(function* () {
   try {
-    yield KowaFs.writeFileAsync('/tmp/f1', 'content');
-    yield KowaFs.appendFileAsync('/tmp/f2', 'content');
-    yield KowaFs.ftruncateAsync('/tmp/f1', 5);
-    yield KowaFs.truncateAsync('/tmp/f1', 1);
+    //yield KowaFs.writeFileAsync('/tmp/f1', 'content');
+    //yield KowaFs.appendFileAsync('/tmp/f2', 'content');
+    var files = yield KowaFs.readdirAsync('/tmp/');
+    console.log(files);
     yield KowaFs.renameAsync('/tmp/f1', '/tmp/f2');
+    //console.log(yield KowaFs.readFileAsync('/tmp/f2'));
   } catch (e) {
-    assert.ok(false);
+    console.log(error, e);
+    assert.equal(false, true);
   }
 });
